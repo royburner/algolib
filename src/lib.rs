56983781eb005c8013f5f3edb144fdad2a70f6d9
royburner <roy.burner@gmail.com>
2015-04-02 23:01:03 +0200

    from fixed len array to vec

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,15 @@
-pub fn connected(a:[i32; 3],p:usize,q:usize)->bool{
+pub fn connected(a:&Vec<i32>,p:usize,q:usize)->bool{
 	a[p]==a[q]
 }
 
-pub fn union(a: &mut [i32; 3],p:usize,q:usize){
+pub fn union(mut a:Vec<i32>,p:usize,q:usize)->Vec<i32>{
 	let p_val = a[p];
 	let q_val = a[q];
-	for i in 0..3{
+	for i in 0..a.len(){
 		if a[i]==p_val {
 			a[i]=q_val;
 		}
 	}
+	a
 }
 
--- tests/test.rs
+++ tests/test.rs
@@ -2,24 +2,30 @@ extern crate algolib;
 
 #[test]
 fn test_connected() {
-	let a = [1, 2, 1];
-	assert!(algolib::connected(a,0,2));
+	let a = vec![1, 2, 1];
+	assert!(algolib::connected(&a,0,2));
+}
+
+#[test]
+fn test_connected_bigger() {
+	let a = vec![1, 2, 1, 5, 6, 4, 8, 3, 1, 2];
+	assert!(algolib::connected(&a,0,2));
 }
 
 
 #[test]
 fn test_not_connected() {
-	let a = [1, 2, 1];
-	assert!(!algolib::connected(a,0,1));
+	let a = vec![1, 2, 1];
+	assert!(!algolib::connected(&a,0,1));
 }
 
 #[test]
 fn test_union(){
-	let mut a = [0, 1, 2];
-	assert!(!algolib::connected(a,0,1));
-	algolib::union(&mut a,0,1);
-	assert!(algolib::connected(a,0,1));
-	algolib::union(&mut a,0,2);
-	assert!(algolib::connected(a,0,1));
-	assert!(algolib::connected(a,1,2));
+	let a = vec![0, 1, 2];
+	assert!(!algolib::connected(&a,0,1));
+	let b = algolib::union(a,0,1);
+	assert!(algolib::connected(&b,0,1));
+	let c = algolib::union(b,0,2);
+	assert!(algolib::connected(&c,0,1));
+	assert!(algolib::connected(&c,1,2));
 }
