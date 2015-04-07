2bc2f9974018afa2a96ce843e916c44dcc90dae5
royburner <roy.burner@gmail.com>
2015-04-07 22:02:57 +0200

    add selection sort

--- src/selection_sort.rs
+++ src/selection_sort.rs
@@ -12,9 +12,17 @@ impl Board{
 		self.val[i]
 	}
 	
-	pub fn sort(&self){
-		for i in 0..self.val.len(){
-			
+	pub fn sort(&mut self){
+		for i in 0..self.val.len()-1{
+			let mut min_index = i;
+			for j in i..self.val.len(){
+				if self.val[j] < self.val[min_index]{
+					min_index = j;
+				}
+			}
+			let ival = self.val[i];
+			self.val[i] = self.val[min_index];
+			self.val[min_index] = ival;
 		}
 	}
 
--- tests/test_sort.rs
+++ tests/test_sort.rs
@@ -7,3 +7,14 @@ fn test_selection_sort_from(){
 	let a = selection_sort::Board::from(vec![0,1,2,3]);
 	assert!(a.val(0)==0);
 }
+
+#[test]
+fn test_selection_sort_sort(){
+	let mut a = selection_sort::Board::from(vec![0,3,2,1]);
+	a.sort();
+
+	assert_eq!(a.val(0),0);
+	assert_eq!(a.val(1),1);
+	assert_eq!(a.val(2),2);
+	assert_eq!(a.val(3),3);
+}
