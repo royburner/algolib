7660d6837ce9c4d350bf6a85a6084b7ed8953a7d
royburner <roy.burner@gmail.com>
2015-09-13 21:24:17 +0200

    adding shell sort for vec

---
+++ src/multi_sort.rs
@@ -0,0 +1,49 @@
+﻿use std::vec::Vec;
+
+/// 
+/// Multiple sort hability
+///
+pub trait MultiSort{
+	/// selection sort algorithm
+	fn sel_sort(&mut self);
+	
+	/// shell sort algorithm
+	fn shell_sort(&mut self);
+}
+
+impl<T:PartialOrd> MultiSort for Vec<T>{
+	fn sel_sort(&mut self){
+		for i in 0..self.len()-1{
+			let mut min_index = i;
+			for j in i..self.len(){
+				if self[j] < self[min_index]{
+					min_index = j;
+				}
+			}
+			self.swap(i, min_index);
+		}
+	}
+	
+	fn shell_sort(&mut self){
+		//determines 3x+1 intervall
+		let mut x = 0;
+		while 3*x + 1 < self.len(){
+			x+=1;
+		}
+		
+		//chaining h sort on decreasing h intervall
+		for i in (0..x).rev() {
+			let h = 3 * i + 1;
+			
+			for j in h..self.len(){
+				let mut k = j;
+				while k > 0 && self[k] < self[k-h]{
+					self.swap(k, k-h);
+					k -= h;
+				}
+			}
+		}
+	}
+}
+
+
--- tests/test_sort.rs
+++ tests/test_sort.rs
@@ -1,7 +1,7 @@
 ﻿extern crate algolib;
 
 use algolib::*;
-use algolib::vec_selection_sort::SelSort;
+use algolib::multi_sort::MultiSort;
 
 #[test]
 fn test_selection_sort_from(){
@@ -21,9 +21,20 @@ fn test_selection_sort_sort(){
 }
 
 #[test]
-fn test_vec_selection_sort_sort(){
+fn test_multi_sort_sel_sort(){
 	let mut a = vec![0,3,2,1];
 	a.sel_sort();
+
+	assert_eq!(a[0],0);
+	assert_eq!(a[1],1);
+	assert_eq!(a[2],2);
+	assert_eq!(a[3],3);
+}
+
+#[test]
+fn test_multi_sort_shell_sort(){
+	let mut a = vec![0,3,2,1];
+	a.shell_sort();
 
 	assert_eq!(a[0],0);
 	assert_eq!(a[1],1);
