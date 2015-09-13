6d435e995638a57868798d0e7cb7c902e8294f0e
royburner <roy.burner@gmail.com>
2015-09-13 21:45:57 +0200

    deleting deprec algo

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,3 @@
 ﻿pub mod quick_find;
 pub mod quick_union;
-pub mod selection_sort;
 pub mod multi_sort;
-pub mod shell_sort;
--- tests/test_sort.rs
+++ tests/test_sort.rs
@@ -4,23 +4,6 @@ use algolib::*;
 use algolib::multi_sort::MultiSort;
 
 #[test]
-fn test_selection_sort_from(){
-	let a = selection_sort::Board::from(vec![0,1,2,3]);
-	assert!(a.val(0)==0);
-}
-
-#[test]
-fn test_selection_sort_sort(){
-	let mut a = selection_sort::Board::from(vec![0,3,2,1]);
-	a.sort();
-
-	assert_eq!(a.val(0),0);
-	assert_eq!(a.val(1),1);
-	assert_eq!(a.val(2),2);
-	assert_eq!(a.val(3),3);
-}
-
-#[test]
 fn test_multi_sort_sel_sort(){
 	let mut a = vec![0,3,2,1];
 	a.sel_sort();
@@ -42,14 +25,4 @@ fn test_multi_sort_shell_sort(){
 	assert_eq!(a[3],3);
 }
 
-#[test]
-fn test_shell_sort_sort(){
-	let mut a = shell_sort::Board::from(vec![0,3,2,1]);
-	a.sort();
-
-	assert_eq!(a.val(0),0);
-	assert_eq!(a.val(1),1);
-	assert_eq!(a.val(2),2);
-	assert_eq!(a.val(3),3);
-}
 
